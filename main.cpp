db35ebfe9ad39033105481c211cac3b8b12e3e99
Samip Shrestha <samipstha55@gmail.com>
2025-07-28 19:28:46 +0545

    Code pushed by Samip

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include "student.h"
-int main() {
-    // Load data from CSV
-    // Menu:
-    // 1. Search
-    // 2. Add
-    // 3. Delete
-    // 4. Update
-    // 5. Save & Exit
+// #include "student.h"
+// int main() {
+//     // Load data from CSV
+//     // Menu:
+//     // 1. Search
+//     // 2. Add
+//     // 3. Delete
+//     // 4. Update
+//     // 5. Save & Exit
+// }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello buddy!";
+    return 0;
 }
